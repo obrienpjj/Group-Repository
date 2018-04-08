4f31c31d7ffedab8676d639e31c4fa370bc0e3b1
obrienpjj <obrienpjj@gmail.com>
2018-04-08 11:15:59 +0100

    Copied Keiths' updates

---
+++ SampleTemplate/Controllers/BookingController.cs
@@ -0,0 +1,24 @@
+﻿using StudioBookingApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleTemplate.Controllers
+{
+    public class BookingController : Controller
+    {
+        // GET: Booking
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Book(Reservation reservation)
+        {
+            return View();
+        }
+    }
+}
